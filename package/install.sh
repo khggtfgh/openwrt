51686d9e71ee84e71a9eb9ef54ef88772353710f
khggtfgh <52488799+khggtfgh@users.noreply.github.com>
2021-07-03 12:37:21 +0800

    Add files via upload

---
+++ package/install.sh
@@ -0,0 +1,5 @@
+opkg install libubox-lua_2020-05-25-66195aee-1_x86_64.ipk
+opkg install kmod-oaf_5.4.79\+3.0-1_x86_64.ipk
+opkg install appfilter_3.0-1_x86_64.ipk
+opkg install luci-app-oaf_3.0-1_all.ipk
+opkg install luci-i18n-oaf-zh-cn_3.0-1_all.ipk
